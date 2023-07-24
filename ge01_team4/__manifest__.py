b1390c680ce95e08f10df5c78d91225a90152514
BNEB <bneb@odoo.com>
2023-07-24 17:11:37 -0600

    [FIX] Added manifest

---
+++ ge01_team4/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": "ge01_team4",
+    
+    "summary": "Module #1",
+    
+    "description": """
+    Create a filter for the products page that only shows products that are a motorcycle type
+   """,
+   'license': 'OPL-1',
+
+    'data': [
+    'views/ge01_product_template_views.xml',
+    ],
+    "depends": ["motorcycle_registry","sale"],
+    
+    "author": "odoo-team4",
+    
+    "website": "www.odoo.com",
+    
+    "application": False,
+    
+}
+
