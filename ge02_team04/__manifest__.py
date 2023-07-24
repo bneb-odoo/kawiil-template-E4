b854717c5daa54be58ecfd4ccb6c5213f9281176
jcsg-odoo <jcsg@odoo.com>
2023-07-24 17:52:41 -0600

    [ADD] Created ge02_team04 module

---
+++ ge02_team04/__manifest__.py
@@ -0,0 +1,31 @@
+{
+    "name": "Motorcycle Registry : Map View",
+    
+    "summary": "Map View extension for Motorcycle Registry",
+    
+    "description": """ Desc """,
+    
+    "version": "0.1",
+    
+    "category": "Kauil/Registry",
+    
+    "license": "OPL-1",
+    
+    "depends": ["motorcycle_registry"],
+    
+    "data": [
+        "views/motorcycle_registry_views_inherit.xml",
+    ],
+    
+    "demo": [
+        "demo/motorcycle_registry_demo.xml",
+        "demo/product_demo.xml",
+    ],
+    
+    "author": "kauil-motors",
+    
+    "website": "www.odoo.com",
+    
+    "application": False,
+    
+}
