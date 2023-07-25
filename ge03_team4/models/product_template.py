3ccfd568562e635b4e0e50069f15403c463102c2
BNEB <bneb@odoo.com>
2023-07-25 15:22:57 -0600

    [FIX]Fixed error that eliminated name field for all non motorcycle products

--- ge03_team4/models/product_template.py
+++ ge03_team4/models/product_template.py
@@ -3,26 +3,25 @@ from odoo import api, fields, models
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
  
-    name = fields.Char(string="Name", index='trigram', required=True,
-                        store=True, translate=True, compute='_compute_NAME')
+    name = fields.Char(string="Name", store=True, compute='_compute_NAME', readonly=False)
 
     @api.depends('make','model','year')
     def _compute_NAME(self):
         name_computed = ''
         for product in self:
             if product.detailed_type == 'motorcycle':
-                if product.make != None:
+                if product.make != False :
                     name_computed += product.make
-                if product.model != None:
+                if product.model != False:
                     name_computed += product.model
-                if product.year != None:
+                if product.year != False:
                     name_computed += str(product.year)
                 if name_computed != '':
                     product.name = name_computed
                 else:
-                    product.name = 'Unnamedmotorcycle'
+                    product.name = False
             else:
-                if product.name not in {'', None}:
+                if product.name != False:
                     product.name = product.name   
                 else:
-                    product.name = 'Unnamedproduct'     
+                    product.name = False   
