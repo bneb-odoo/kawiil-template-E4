74c85b777a57786d80a985cec753d6633ff95c36
jcsg-odoo <jcsg@odoo.com>
2023-07-25 10:24:16 -0600

    [ADD] Updated ge02_team04 manifest format and content

--- ge02_team04/__manifest__.py
+++ ge02_team04/__manifest__.py
@@ -1,31 +1,20 @@
 {
     "name": "Motorcycle Registry : Map View",
-    
     "summary": "Map View extension for Motorcycle Registry",
-    
-    "description": """ Desc """,
-    
+    "description": """ 
+    Motorcycle Registry : Map View \n
+    Task id: 3426889 \n
+    Dev: team4 \n
+    This module adds a map view to de registry action window of the Motorcycle Registry Module.""",
     "version": "0.1",
-    
     "category": "Kauil/Registry",
-    
     "license": "OPL-1",
-    
     "depends": ["motorcycle_registry"],
-    
     "data": [
         "views/motorcycle_registry_views_inherit.xml",
     ],
-    
-    "demo": [
-        "demo/motorcycle_registry_demo.xml",
-        "demo/product_demo.xml",
-    ],
-    
-    "author": "kauil-motors",
-    
+    "demo": [],
+    "author": "Odoo, Inc",
     "website": "www.odoo.com",
-    
     "application": False,
-    
 }
