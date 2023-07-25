95d3bb70edf146da063d78211cfccb7112a718c2
BNEB <bneb@odoo.com>
2023-07-25 10:10:26 -0600

    [IMPV] Improved manifest

--- ge01_team4/__manifest__.py
+++ ge01_team4/__manifest__.py
@@ -1,23 +1,14 @@
 {
     "name": "ge01_team4",
-    
-    "summary": "Module #1",
-    
-    "description": """
-    Create a filter for the products page that only shows products that are a motorcycle type
-   """,
+    "summary": "Default motorcycle filter for products",
+    "description": """Filters the products page to show products that are a motorcycle type 
+    Task Id:342602 Dev: Odoo Team 4""",
    'license': 'OPL-1',
-
     'data': [
     'views/ge01_product_template_views.xml',
     ],
     "depends": ["motorcycle_registry","sale"],
-    
-    "author": "odoo-team4",
-    
+    "author": "Odoo,Inc",
     "website": "www.odoo.com",
-    
     "application": False,
-    
 }
-
