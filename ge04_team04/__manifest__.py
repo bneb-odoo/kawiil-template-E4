aa5982ef7141345726dbe1efff2d32f187533a67
jcsg-odoo <jcsg@odoo.com>
2023-07-25 10:37:02 -0600

    [ADD] Added ge04_team04 module

---
+++ ge04_team04/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Motorcycle Registry : Customer Discount",
+    "summary": "Give a $2500 discount to new customers",
+    "description": """ 
+    Motorcycle Registry : Customer Discount \n
+    This module allows you to give a $2500 discount to new customers purchasing an electric motorcycle for the first time.
+    Task id: 3426895 \n
+    Dev: team4 """,
+    "version": "0.1",
+    "category": "Kauil/Registry",
+    "license": "OPL-1",
+    "depends": ["motorcycle_registry"],
+    "data": [],
+    "demo": [],
+    "author": "Odoo, Inc",
+    "website": "www.odoo.com",
+    "application": False,
+}
