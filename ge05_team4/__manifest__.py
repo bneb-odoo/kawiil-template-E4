e0da1ddc2b749133ac0313fc2936dd28a36a0808
BNEB <bneb@odoo.com>
2023-07-26 11:55:37 -0600

    [ADD] Added full lists of states

---
+++ ge05_team4/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "ge05_team04",
+    "summary": "Distance based warehouse computation on SO",
+    "description": """Changes Sales Orders so that the nearest existing warehouse
+    to the specified delivery order is assigned to it automaticaly. 
+    Task Id:3427342 Dev: Odoo Team 4""",
+   'license': 'OPL-1',
+    "version": "1",
+    "depends": ["sale_stock"],
+    "author": "Odoo,Inc.",
+    "website": "www.odoo.com",
+    "application": False,
+}
